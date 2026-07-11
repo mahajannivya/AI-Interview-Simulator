2b0ee0a7312cc51f082884f83ada2f53d2f2daf8
nivya <mahajannivya@gmail.com>
2026-07-11 11:39:36 +0530

    Added util class for the Constants variables

---
+++ src/main/java/com/interview/simulator/util/Constants.java
@@ -0,0 +1,8 @@
+package com.interview.simulator.util;
+
+public class Constants {
+    public static final int MAX_QUESTIONS_PER_SESSION = 10;
+    public static final int MIN_SCORE = 1;
+    public static final int MAX_SCORE = 10;
+    public static final String CUSTOM_CATEGORY_NAME = "Custom Topic";
+}
